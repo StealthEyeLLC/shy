42233d491566a0d6d0c521b57ef2a6bb5c34c4bb
Jamie Currier <odinsravenxx@gmail.com>
2025-12-12 23:41:08 -0500

    v0.9.0 pass1: cli refusal reporting structure

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod api;
 pub mod flow;
+pub mod report;
 
 pub use api::*;
 pub use flow::*;
+pub use report::*;
---
+++ target/debug/deps/cli-2ef278588d09a6ec.d
@@ -0,0 +1,8 @@
+C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\cli-2ef278588d09a6ec.d: cli\src\lib.rs cli\src\api.rs cli\src\flow.rs cli\src\report.rs
+
+C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\libcli-2ef278588d09a6ec.rmeta: cli\src\lib.rs cli\src\api.rs cli\src\flow.rs cli\src\report.rs
+
+cli\src\lib.rs:
+cli\src\api.rs:
+cli\src\flow.rs:
+cli\src\report.rs:
