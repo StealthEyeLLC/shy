97c985a596722eac9ed22a81c7efbb710e8ab708
Jamie Currier <odinsravenxx@gmail.com>
2025-12-12 23:01:10 -0500

    v0.5.0 pass1: coordinator enforcement structure

--- coordinator/src/lib.rs
+++ coordinator/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod api;
 pub mod flow;
 pub mod errors;
+pub mod enforcement;
 
 pub use api::*;
 pub use flow::*;
--- target/debug/deps/coordinator-d7e96bf40a6b7478.d
+++ target/debug/deps/coordinator-d7e96bf40a6b7478.d
@@ -1,8 +1,9 @@
-C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\coordinator-d7e96bf40a6b7478.d: coordinator\src\lib.rs coordinator\src\api.rs coordinator\src\flow.rs coordinator\src\errors.rs
+C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\coordinator-d7e96bf40a6b7478.d: coordinator\src\lib.rs coordinator\src\api.rs coordinator\src\flow.rs coordinator\src\errors.rs coordinator\src\enforcement.rs
 
-C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\libcoordinator-d7e96bf40a6b7478.rmeta: coordinator\src\lib.rs coordinator\src\api.rs coordinator\src\flow.rs coordinator\src\errors.rs
+C:\Users\odins\Desktop\stealtheyellc\shy\target\debug\deps\libcoordinator-d7e96bf40a6b7478.rmeta: coordinator\src\lib.rs coordinator\src\api.rs coordinator\src\flow.rs coordinator\src\errors.rs coordinator\src\enforcement.rs
 
 coordinator\src\lib.rs:
 coordinator\src\api.rs:
 coordinator\src\flow.rs:
 coordinator\src\errors.rs:
+coordinator\src\enforcement.rs:
